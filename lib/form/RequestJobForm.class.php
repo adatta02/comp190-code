4c5d4b33c432ed46ef3b3fd4215e73af4165d247
Alissa Cooper <alissa.cooper@tufts.edu>
2009-04-11 14:29:52 -0400

    changed job request form

--- lib/form/RequestJobForm.class.php
+++ lib/form/RequestJobForm.class.php
@@ -11,7 +11,39 @@
 
 class RequestJobForm extends sfForm {
 	public function setup() {
-		$this->setWidgets ( array ('id' => new sfWidgetFormInputHidden ( ), 'project_id' => new sfWidgetFormPropelChoice ( array ('model' => 'Project', 'add_empty' => true ) ), 'publication_id' => new sfWidgetFormPropelChoice ( array ('model' => 'Publication', 'add_empty' => true ) ), 'status_id' => new sfWidgetFormPropelChoice ( array ('model' => 'Status', 'add_empty' => true ) ), 'event' => new sfWidgetFormInput ( ), 'date' => new sfWidgetFormJQueryDate ( ), 'start_time' => new sfWidgetjQueryTimepickr ( array ( ), array ("size" => 7 ) ), 'end_time' => new sfWidgetjQueryTimepickr ( array ( ), array ("size" => 7 ) ), 'due_date' => new sfWidgetFormJQueryDate ( ), 'created_at' => new sfWidgetFormDateTime ( ), 'street' => new sfWidgetFormInput ( ), 'city' => new sfWidgetFormInput ( ), 'state' => new sfWidgetFormSelectUSState ( ), 'zip' => new sfWidgetFormInput ( ), 'contact_name' => new sfWidgetFormInput ( ), 'contact_email' => new sfWidgetFormInput ( ), 'contact_phone' => new sfWidgetFormInput ( ), 'notes' => new sfWidgetFormTextarea ( ), 'estimate' => new sfWidgetFormInput ( ), 'photo_type' => new sfWidgetFormInput ( ), 'acct_num' => new sfWidgetFormInput ( ), 'dept_id' => new sfWidgetFormInput ( ), 'grant_id' => new sfWidgetFormInput ( ), 'other' => new sfWidgetFormInput ( ), 'slug' => new sfWidgetFormInput ( ), 'user_id' => new sfWidgetFormPropelChoice ( array ('model' => 'sfGuardUserProfile', 'add_empty' => true ) ), 'name' => new sfWidgetFormInput ( ), 'department' => new sfWidgetFormInput ( ), 'address' => new sfWidgetFormInput ( ), 'email' => new sfWidgetFormInput ( ), 'phone' => new sfWidgetFormInput ( ) ) );
+		$this->setWidgets ( array (
+		 'id' => new sfWidgetFormInputHidden ( ), 
+		'project_id' => new sfWidgetFormPropelChoice ( array ('model' => 'Project', 'add_empty' => true ) ), 
+		'publication_id' => new sfWidgetFormPropelChoice ( array ('model' => 'Publication', 'add_empty' => true ) ),
+		'status_id' => new sfWidgetFormPropelChoice ( array ('model' => 'Status', 'add_empty' => true ) ), 
+		'event' => new sfWidgetFormInput ( ), 
+		'date' => new sfWidgetFormJQueryDate ( ), 
+		'start_time' => new sfWidgetjQueryTimepickr ( array ( ), array ("size" => 7 ) ), 
+		'end_time' => new sfWidgetjQueryTimepickr ( array ( ), array ("size" => 7 ) ), 
+		'due_date' => new sfWidgetFormJQueryDate ( ), 
+		'created_at' => new sfWidgetFormDateTime ( ), 
+		'street' => new sfWidgetFormInput ( ), 
+		'city' => new sfWidgetFormInput ( ), 
+		'state' => new sfWidgetFormSelectUSState ( ), 
+		'zip' => new sfWidgetFormInput ( ), 
+		'contact_name' => new sfWidgetFormInput ( ), 
+		'contact_email' => new sfWidgetFormInput ( ), 
+		'contact_phone' => new sfWidgetFormInput ( ), 
+		'notes' => new sfWidgetFormTextarea ( ), 
+		'estimate' => new sfWidgetFormInput ( ), 
+		'photo_type' => new sfWidgetFormSelectCheckbox ( ), 
+		'acct_num' => new sfWidgetFormInput ( ), 
+		'dept_id' => new sfWidgetFormInput ( ), 
+		'grant_id' => new sfWidgetFormInput ( ), 
+		'other' => new sfWidgetFormInput ( ), 
+		'slug' => new sfWidgetFormInput ( ), 
+		'user_id' => new sfWidgetFormPropelChoice ( array ('model' => 'sfGuardUserProfile', 'add_empty' => true ) ), 
+		'name' => new sfWidgetFormInput ( ), 
+		'department' => new sfWidgetFormInput ( ), 
+		'address' => new sfWidgetFormInput ( ), 
+		'email' => new sfWidgetFormInput ( ), 
+		'phone' => new sfWidgetFormInput ( ) 
+	) );
 		
 		$this->widgetSchema->setLabel ( 'event', 'Event Name' );
 		$this->widgetSchema->setLabel ( 'publication_id', 'Publication' );
@@ -24,7 +56,39 @@ class RequestJobForm extends sfForm {
 		$this->widgetSchema ['now'] = new sfWidgetFormInputHidden ( );
 		$this->widgetSchema ['now']->setDefault ( time () );
 		
-		$this->setValidators ( array ('id' => new sfValidatorPropelChoice ( array ('model' => 'Job', 'column' => 'id', 'required' => false ) ), 'project_id' => new sfValidatorPropelChoice ( array ('model' => 'Project', 'column' => 'id', 'required' => false ) ), 'publication_id' => new sfValidatorPropelChoice ( array ('model' => 'Publication', 'column' => 'id', 'required' => false ) ), 'status_id' => new sfValidatorPropelChoice ( array ('model' => 'Status', 'column' => 'id', 'required' => false ) ), 'event' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 'date' => new sfValidatorDate ( array ('required' => false ) ), 'start_time' => new sfValidatorDateTime ( array ('required' => false ) ), 'end_time' => new sfValidatorDateTime ( array ('required' => false ) ), 'due_date' => new sfValidatorDateTime ( array ('required' => false ) ), 'created_at' => new sfValidatorDateTime ( array ('required' => false ) ), 'street' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 'city' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 'state' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 'zip' => new sfValidatorInteger ( array ('required' => false ) ), 'contact_name' => new sfValidatorString ( array ('max_length' => 45, 'required' => false ) ), 'contact_email' => new sfValidatorEmail ( array ('required' => true ) ), 'contact_phone' => new sfValidatorString ( array ('max_length' => 45, 'required' => false ) ), 'notes' => new sfValidatorString ( array ('required' => false ) ), 'estimate' => new sfValidatorInteger ( array ('required' => false ) ), 'photo_type' => new sfValidatorInteger ( array ('required' => false ) ), 'acct_num' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ), 'dept_id' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ), 'grant_id' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ), 'other' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 'slug' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 'user_id' => new sfValidatorPropelChoice ( array ('model' => 'sfGuardUserProfile', 'column' => 'id', 'required' => false ) ), 'name' => new sfValidatorString ( array ('max_length' => 45, 'required' => false ) ), 'department' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 'address' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 'email' => new sfValidatorEmail ( array ('required' => true ) ), 'phone' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ) ) );
+		$this->setValidators ( array (
+			'id' => new sfValidatorPropelChoice ( array ('model' => 'Job', 'column' => 'id', 'required' => false ) ),
+		 	'project_id' => new sfValidatorPropelChoice ( array ('model' => 'Project', 'column' => 'id', 'required' => false ) ), 
+			'publication_id' => new sfValidatorPropelChoice ( array ('model' => 'Publication', 'column' => 'id', 'required' => false ) ), 
+			'status_id' => new sfValidatorPropelChoice ( array ('model' => 'Status', 'column' => 'id', 'required' => false ) ), 
+			'event' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 
+			'date' => new sfValidatorDate ( array ('required' => false ) ), 
+			'start_time' => new sfValidatorDateTime ( array ('required' => false ) ), 
+			'end_time' => new sfValidatorDateTime ( array ('required' => false ) ), 
+			'due_date' => new sfValidatorDateTime ( array ('required' => false ) ), 
+			'created_at' => new sfValidatorDateTime ( array ('required' => false ) ), 
+			'street' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 
+			'city' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 
+			'state' => new sfValidatorString ( array ('max_length' => 64, 'required' => false ) ), 
+			'zip' => new sfValidatorInteger ( array ('required' => false ) ), 
+			'contact_name' => new sfValidatorString ( array ('max_length' => 45, 'required' => false ) ), 
+			'contact_email' => new sfValidatorEmail ( array ('required' => true ) ), 
+			'contact_phone' => new sfValidatorString ( array ('max_length' => 45, 'required' => false ) ), 
+			'notes' => new sfValidatorString ( array ('required' => false ) ), 
+			'estimate' => new sfValidatorInteger ( array ('required' => false ) ), 
+			'photo_type' => new sfValidatorInteger ( array ('required' => false ) ), 
+			'acct_num' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ), 
+			'dept_id' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ), 
+			'grant_id' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ), 
+			'other' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 
+			'slug' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 
+			'user_id' => new sfValidatorPropelChoice ( array ('model' => 'sfGuardUserProfile', 'column' => 'id', 'required' => false ) ), 
+			'name' => new sfValidatorString ( array ('max_length' => 45, 'required' => false ) ), 
+			'department' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 
+			'address' => new sfValidatorString ( array ('max_length' => 255, 'required' => false ) ), 
+			'email' => new sfValidatorEmail ( array ('required' => true ) ), 
+			'phone' => new sfValidatorString ( array ('max_length' => 32, 'required' => false ) ) 
+	) );
 		
 		$this->validatorSchema ['now'] = new sfValidatorDate ( );
 		$this->validatorSchema ['event']->setOption ( 'required', true );
