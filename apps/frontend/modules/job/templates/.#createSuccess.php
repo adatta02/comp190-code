b87b7d8f9c2509e7d11c885273c7219e11ddb095
Alissa Cooper <alissa.cooper@tufts.edu>
2009-04-21 21:06:07 -0400

    cleaning

--- apps/frontend/modules/job/templates/.#createSuccess.php
+++
@@ -1,1 +0,0 @@
-acoope05@d-p-13-72.eecs.tufts.edu.16155:1235336965
--- lib/form/base/BasePhotographerForm.class.php
+++ lib/form/base/BasePhotographerForm.class.php
@@ -13,27 +13,29 @@ class BasePhotographerForm extends BaseFormPropel
   public function setup()
   {
     $this->setWidgets(array(
-      'id'          => new sfWidgetFormInputHidden(),
-      'user_id'     => new sfWidgetFormPropelChoice(array('model' => 'sfGuardUserProfile', 'add_empty' => true)),
-      'name'        => new sfWidgetFormInput(),
-      'phone'       => new sfWidgetFormInput(),
-      'email'       => new sfWidgetFormInput(),
-      'affiliation' => new sfWidgetFormInput(),
-      'website'     => new sfWidgetFormInput(),
-      'description' => new sfWidgetFormTextarea(),
-      'slug'        => new sfWidgetFormInput(),
+      'id'              => new sfWidgetFormInputHidden(),
+      'user_id'         => new sfWidgetFormPropelChoice(array('model' => 'sfGuardUserProfile', 'add_empty' => true)),
+      'name'            => new sfWidgetFormInput(),
+      'phone'           => new sfWidgetFormInput(),
+      'email'           => new sfWidgetFormInput(),
+      'affiliation'     => new sfWidgetFormInput(),
+      'website'         => new sfWidgetFormInput(),
+      'description'     => new sfWidgetFormTextarea(),
+      'billing_address' => new sfWidgetFormTextarea(),
+      'slug'            => new sfWidgetFormInput(),
     ));
 
     $this->setValidators(array(
-      'id'          => new sfValidatorPropelChoice(array('model' => 'Photographer', 'column' => 'id', 'required' => false)),
-      'user_id'     => new sfValidatorPropelChoice(array('model' => 'sfGuardUserProfile', 'column' => 'id', 'required' => false)),
-      'name'        => new sfValidatorString(array('max_length' => 64, 'required' => false)),
-      'phone'       => new sfValidatorString(array('max_length' => 45, 'required' => false)),
-      'email'       => new sfValidatorString(array('max_length' => 64, 'required' => false)),
-      'affiliation' => new sfValidatorString(array('max_length' => 64, 'required' => false)),
-      'website'     => new sfValidatorString(array('max_length' => 64, 'required' => false)),
-      'description' => new sfValidatorString(array('required' => false)),
-      'slug'        => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'id'              => new sfValidatorPropelChoice(array('model' => 'Photographer', 'column' => 'id', 'required' => false)),
+      'user_id'         => new sfValidatorPropelChoice(array('model' => 'sfGuardUserProfile', 'column' => 'id', 'required' => false)),
+      'name'            => new sfValidatorString(array('max_length' => 64, 'required' => false)),
+      'phone'           => new sfValidatorString(array('max_length' => 45, 'required' => false)),
+      'email'           => new sfValidatorString(array('max_length' => 64, 'required' => false)),
+      'affiliation'     => new sfValidatorString(array('max_length' => 64, 'required' => false)),
+      'website'         => new sfValidatorString(array('max_length' => 64, 'required' => false)),
+      'description'     => new sfValidatorString(array('required' => false)),
+      'billing_address' => new sfValidatorString(array('required' => false)),
+      'slug'            => new sfValidatorString(array('max_length' => 255, 'required' => false)),
     ));
 
     $this->widgetSchema->setNameFormat('photographer[%s]');
