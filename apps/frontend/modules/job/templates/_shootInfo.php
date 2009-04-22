4d186cb13dabd71ee50930e63d016af877fae664
Alissa Cooper <alissa.cooper@tufts.edu>
2009-04-22 10:07:16 -0400

    added in google directions not 100% done

--- apps/frontend/modules/job/templates/_shootInfo.php
+++ apps/frontend/modules/job/templates/_shootInfo.php
@@ -27,7 +27,49 @@
       var map = new GMap2(document.getElementById("map"));
       var geocoder = new GClientGeocoder();
       map.setUIToDefault();
-      
+
+      var gmarkers = [];
+      var htmls = [];
+      var to_htmls = [];
+      var from_htmls = [];
+      var i=0;
+
+      function createMarker(point,html) {
+        var marker = new GMarker(point);
+
+	to_htmls[i] = html + '<br>Directions: <b>To here<\/b> - <a href="javascript:fromhere(' + i + ')">From here<\/a>' +
+           '<br>Start address:<form action="http://maps.google.com/maps" method="get" target="_blank">' +
+           '<input type="text" SIZE=40 MAXLENGTH=40 name="saddr" id="saddr" value="" /><br>' +
+           '<INPUT value="Get Directions" TYPE="SUBMIT">' +
+           '<input type="hidden" name="daddr" value="' + point.lat() + ',' + point.lng() +
+           '"/>';
+   
+	from_htmls[i] = html + '<br>Directions: <a href="javascript:tohere(' + i + ')">To here<\/a> - <b>From here<\/b>' +
+           '<br>End address:<form action="http://maps.google.com/maps" method="get"" target="_blank">' +
+           '<input type="text" SIZE=40 MAXLENGTH=40 name="daddr" id="daddr" value="" /><br>' +
+           '<INPUT value="Get Directions" TYPE="SUBMIT">' +
+           '<input type="hidden" name="saddr" value="' + point.lat() + ',' + point.lng() + 
+	   '"/>';
+
+	   html = html + '<br>Directions: <a href="javascript:tohere('+i+')">To here<\/a> - <a href="javascript:fromhere('+i+')">From here<\/a>';
+	 
+        GEvent.addListener(marker, "click", function() {
+          marker.openInfoWindowHtml(html);
+        });
+
+        htmls[i] = html;
+        i++;
+        return marker;
+      }
+
+      function tohere(i) {
+        marker.openInfoWindowHtml(to_htmls[i]);
+      }
+
+      function fromhere(i) {
+        marker.openInfoWindowHtml(from_htmls[i]);
+      }
+  
       var address = "<?php echo $job->getStreet(); ?>,<?php echo $job->getCity(); ?>,<?php echo $job->getState(); ?>";
                   geocoder.getLatLng(
                   address,
@@ -36,9 +78,9 @@
                         alert(address + " not found");
                     } else {
                         map.setCenter(point, 13);
-                        var marker = new GMarker(point);
-                        map.addOverlay(marker);
-                        marker.openInfoWindowHtml(address);
+                        var marker = createMarker(point, address);
+			map.addOverlay(marker);
+			marker.openInfoWindowHtml(html);
                     }
                   }
                 );
